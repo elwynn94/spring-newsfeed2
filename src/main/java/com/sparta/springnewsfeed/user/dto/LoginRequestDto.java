2271eab94bfbab47d37575014676b5d13fe47a0c
Dongseon <elwynn94@gmail.com>
2024-06-16 21:40:39 +0900

    Feat : Jacoco 추가, FixtureMonkey 추가& Test

--- src/main/java/com/sparta/springnewsfeed/user/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/springnewsfeed/user/dto/LoginRequestDto.java
@@ -2,9 +2,11 @@ package com.sparta.springnewsfeed.user.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Value;
 
 @Setter
 @Getter
+@Value
 public class LoginRequestDto {
     private String userId;
     private String password;
